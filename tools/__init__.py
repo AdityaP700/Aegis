ae8cab0edf16e2470f05294119e51ef255b61922
AdityaP700 <126982848+AdityaP700@users.noreply.github.com>
2026-08-09 13:17:34 +0530

    add(init_tool):search tool was missing ,added

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,6 @@
 """centralised service to initialized the different tools"""
 from tools.calculator import CalculatorTool
 from tools.weather import WeatherTool
-from tools.github import GitHubTool      # ← ADD THIS
-
-__all__ = ["CalculatorTool", "WeatherTool", "GitHubTool"]
+from tools.github import GitHubTool
+from tools.search_tool import SearchTool
+__all__ = ["CalculatorTool", "WeatherTool", "GitHubTool","SearchTool"]
